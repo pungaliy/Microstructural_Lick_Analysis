9d40fdea7e2c98237a40c778b0f7755553585382
kbchandr <kbchandr@usc.edu>
2018-03-08 13:42:36 -0800

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='microstructural_lick_analysis',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/pungaliy/Microstructural_Lick_Analysis.git',
+      author='Shray Pungaliya',
+      author_email='flyingcircus@example.com',
+      packages=['microstructural_lick_analysis'],
+      zip_safe=False)
